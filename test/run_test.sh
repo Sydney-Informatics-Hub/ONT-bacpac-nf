70561ee13b0fef6f5710673712d1ac4fc5c3ecea
georgiesamaha <georgiesamaha@gmail.com>
2024-07-23 16:53:18 +1000

    Add gadi account and storage to run script

--- test/run_test.sh
+++ test/run_test.sh
@@ -22,6 +22,8 @@ k2db= #path to predownloaded kraken2 database
 multiqc_config= #path to multiqc config.yml
 sequencing_summary= #path to sequencing summary file from ONT run 
 pycoqc_header= #path to pycoQC header file .txt
+gadi_account= #e.g. aa00
+gadi_storage= #e.g. scratch/aa00
 
 # Run pipeline 
 nextflow run main.nf \
@@ -30,4 +32,6 @@ nextflow run main.nf \
   --multiqc_config ${multiqc_config} \
   --sequencing_summary ${sequencing_summary} \
   --pycoqc_header_file ${pycoqc_header} \
+  --gadi_account ${gadi_account} \
+  --gadi_storage ${gadi_storage} \
   -resume 
